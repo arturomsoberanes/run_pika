7e30c0e4180c26636864a302b4c4a5a619a830fa
Arturo Soberanes <arturo.soberanes01@gmail.com>
2023-10-08 16:16:20 -0600

    Change var for let, i change the setTimeout for setInterval for not full the call stack and new function for create the interval

--- js/pikachu.js
+++ js/pikachu.js
@@ -1,42 +1,46 @@
-var pos = 0;
+let pos = 0;
 const pikaArray = [
-    ['./img/pika1.png', './img/pika2.png','./img/pika3.png','./img/pika4.png'],
+  ["./img/pika1.png", "./img/pika2.png", "./img/pika3.png", "./img/pika4.png"],
 ];
-var direction = 0;
-var focus = 0;
+let direction = 0;
+let focus = 0;
+let interval = null;
 
-function Run() {
-    let img = document.getElementById("Pikachu");
-    let imgWidth = img.width
-    if(focus === 3){
-      focus = 0;
-    }
-    else{
-      focus += 1;
-    }
-    direction = checkPageBounds(direction, imgWidth);
-    img.src = pikaArray[0][focus];
-    if (direction) {
-        pos -= 20;
-        img.style.left = pos + "px";
-        img.style.transform = "scaleX(-1)";
-    } else {
-        pos += 20;
-        img.style.left = pos + 'px';
-        img.style.transform = "scaleX(1)";
-    }
-    setTimeout(Run,90);
-    console.log(direction);
-}
+const checkPageBounds = (direction, imgWidth) => {
+  let widthScreen = window.innerWidth - imgWidth;
 
-function checkPageBounds(direction, imgWidth) {
-    let widthScreen = window.innerWidth - imgWidth;
+  if (pos >= widthScreen) {
+    direction = 1;
+  } else if (pos === 0) {
+    direction = 0;
+  }
 
-    if(pos >= widthScreen){
-      direction = 1;
-    }else if(pos === 0){
-      direction = 0;
-    }
+  return direction;
+};
 
-    return direction;
-}
+const Run = () => {
+  let img = document.getElementById("Pikachu");
+  let imgWidth = img.width;
+  if (focus === 3) {
+    focus = 0;
+  } else {
+    focus += 1;
+  }
+  direction = checkPageBounds(direction, imgWidth);
+  img.src = pikaArray[0][focus];
+  if (direction) {
+    pos -= 20;
+    img.style.left = pos + "px";
+    img.style.transform = "scaleX(-1)";
+  } else {
+    pos += 20;
+    img.style.left = pos + "px";
+    img.style.transform = "scaleX(1)";
+  }
+  startInterval();
+};
+
+const startInterval = () => {
+  if (interval) return;
+  interval = setInterval(Run, 90);
+};
